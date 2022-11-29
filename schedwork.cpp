4624d7193dd564f070a981264696df33edbbfdbf
Mantej Lamba <mlamba@usc.edu>
2022-11-29 07:59:44 +0000

    changes

--- schedwork.cpp
+++ schedwork.cpp
@@ -61,7 +61,7 @@ bool schedule(
     
 }
 
-bool help(int size, const AvailabilityMatrix &avail, int maxShifts, DailySchedule &sched)
+bool help(int size, const AvailabilityMatrix &av, int shifts, DailySchedule &schedule)
 {
     int rows, columns = 0;
     bool available;
@@ -96,7 +96,7 @@ bool help(int size, const AvailabilityMatrix &avail, int maxShifts, DailySchedul
     return false;
 }
 
-bool placement(const AvailabilityMatrix &avail, int &x, int &y, DailySchedule &sched)
+bool placement(const AvailabilityMatrix &av, int &one, int &two, DailySchedule &schedule)
 {
     x = 0;
     do
@@ -129,7 +129,7 @@ bool placement(const AvailabilityMatrix &avail, int &x, int &y, DailySchedule &s
     return false;
 }
 
-bool confirmFree(size_t dailyNeed, size_t x, size_t section, size_t maxShifts, DailySchedule &sched, const AvailabilityMatrix &avail)
+bool confirmFree(size_t dailyNeed, size_t x, size_t sec, size_t shifts, DailySchedule &schedule, const AvailabilityMatrix &av)
 {
     bool banner;
     int temp = 0;
